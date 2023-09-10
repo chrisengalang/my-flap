f886f71b44fcb201c817f920deec1b9c09099ee0
Chrisen John Abraham Galang <chrisen.galang@gmail.com>
2023-09-10 14:00:36 +0800

    Initial commit for my flappy bird game

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public TMP_Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore++;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
